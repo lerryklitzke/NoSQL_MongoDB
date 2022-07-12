72ce3629d8d9cf8073e44ce71e7003f6dc6726a3
Lerry Klitzke <101411685+lerryklitzke@users.noreply.github.com>
2022-07-12 16:10:02 -0300

    adjusts

--- controllers/testControllers.js
+++ controllers/testControllers.js
@@ -1,5 +1,6 @@
 const mongoConnect = require('../util/test-db');
 const callbacks = require('../models/testModels');
+const url = 'http://localhost:8080';
 
 module.exports.get = async (req, res, next) => {
   const payload = req.query;
@@ -10,21 +11,22 @@ module.exports.get = async (req, res, next) => {
 
 module.exports.post = (req, res, next) => {
   const payload = req.body;
+  console.log(payload);
   const callback = callbacks.post;
   mongoConnect('collection', callback, payload);
-  res.send();
+  res.redirect(url);
 }
 
 module.exports.put = (req, res, next) => {
   const payload = req.body;
   const callback = callbacks.put;
   mongoConnect('collection', callback, payload);
-  res.send(); 
+  res.redirect(url); 
 }
 
 module.exports.delete = (req, res, next) => {
-  const payload = req.query;
+  const payload = req.body;
   const callback = callbacks.delete;
   mongoConnect('collection', callback, payload);
-  res.send();
+  res.redirect(url);
 }
